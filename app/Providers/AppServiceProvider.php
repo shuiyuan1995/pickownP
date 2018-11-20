1df7068653abbb81d9de2a1a4bd6281afeb2a592
huangteng <elleveing@gmail.com>
2018-11-20 17:42:34 +0800

    修改添加数据库是出现的错误。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         \Illuminate\Pagination\LengthAwarePaginator::defaultView('vendor.pagination.default');
     }
 

2b0d80ee481c30aa2e882f3ebe452daead8f0cc1
huangteng <elleveing@gmail.com>
2018-11-22 02:14:33 +0800

    添加数据工厂和自动化数据添加类。

---
+++ database/factories/InPacketFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\InPacket::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/GamePartitionsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GamePartitionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

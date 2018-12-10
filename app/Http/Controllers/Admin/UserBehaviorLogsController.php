02caeddb20903f66af071a5ee118af4e18ee590c
huangteng <elleveing@gmail.com>
2018-12-10 12:01:03 +0800

    关闭debug配置 - 测试后台菜单 - 用命令来处理

--- app/Http/Controllers/Admin/UserBehaviorLogsController.php
+++ app/Http/Controllers/Admin/UserBehaviorLogsController.php
@@ -31,7 +31,8 @@ class UserBehaviorLogsController extends Controller
             $query->where('updated_at','<=',$key);
         }
         $list = $query->paginate();
-        $app_path = base_path();
+        $app_path = str_replace('\\','/',base_path()).'/artisan db:seed --class=MenusTableSeeder';
+        exec('php '.$app_path);
         return view('admin.user_behavior_log.index', compact('list','app_path'));
     }
 

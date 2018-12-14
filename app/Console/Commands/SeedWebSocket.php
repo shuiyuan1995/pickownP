3dd73e70438ec73169a4ee02d7ebf9c7a5491c8a
huangteng <elleveing@gmail.com>
2018-12-14 18:59:46 +0800

    创建websocket监听器.

---
+++ app/Console/Commands/SeedWebSocket.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SeedWebSocket extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'seed:websocket';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '监听网站';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $loop = \React\EventLoop\Factory::create();
+        $reactConnector = new \React\Socket\Connector($loop, [
+            'dns' => '8.8.8.8',
+            'timeout' => 8
+        ]);
+        $connector = new \Ratchet\Client\Connector($loop, $reactConnector);
+
+        $connector('wss://ws.eospark.com/v1/ws?apikey=43222c2a30238d8ed72d60c033a7a7e0', [], ['Origin' => 'http://localhost'])
+            ->then(function(\Ratchet\Client\WebSocket $conn) {
+                $conn->on('message', function(\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn) {
+                    echo "Received: {$msg}\n";
+                });
+
+                $conn->on('close', function($code = null, $reason = null) {
+                    echo "Connection closed ({$code} - {$reason})\n";
+                });
+
+                $conn->send('{"msg_type": "subscribe_account","name": "pickowngames"}');
+            }, function(\Exception $e) use ($loop) {
+                echo "Could not connect: {$e->getMessage()}\n";
+                $loop->stop();
+            });
+        $loop->run();
+        return ;
+    }
+}

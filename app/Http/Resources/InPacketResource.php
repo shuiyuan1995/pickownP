d2a80761d7e79161f770521fa03a81caeb3b1c8c
huangteng <elleveing@gmail.com>
2018-12-15 14:26:14 +0800

    抢红包列表包含尾数信息.

--- app/Http/Resources/InPacketResource.php
+++ app/Http/Resources/InPacketResource.php
@@ -21,6 +21,7 @@ class InPacketResource extends JsonResource
             'outid' => $this->outid,
             'userid' => $this->userid,
             'user' => $this->user->name,
+            'tail_number'=>$this->out->tail_number,
             'eosid' => $this->eosid,
             'blocknumber' => $this->blocknumber,
             'outblocknumber' => data_get($this,'out.blocknumber',0),

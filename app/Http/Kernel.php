6fa571ca7cb61ff44ea52f290d790171c0a007aa
lizesheng <1398978641@qq.com>
2019-01-25 14:01:20 +0800

    no message

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CheckToken::class,
     ];
 
     /**
@@ -44,7 +43,6 @@ class Kernel extends HttpKernel
         'api' => [
             'api_throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
